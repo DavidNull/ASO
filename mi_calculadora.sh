e728ad049ba29d2d0b5c6ec17550027704c092d5
DavidNull <davidcelapedraza@gmail.com>
2024-10-21 12:24:25 +0200

    Modificar mi_calculadora.sh para aceptar tres argumentos

---
+++ mi_calculadora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source funciones_math.sh
+
+if [ "$1" == "s" ]; then
+    resultado=$(suma $2 $3)
+    echo "La suma de $2 y $3 es: $resultado"
+elif [ "$1" == "r" ]; then
+    resultado=$(resta $2 $3)
+    echo "La resta de $2 y $3 es: $resultado"
+else
+    echo "Usa s para suma o r para resta."
+fi
+
